516d41119aaa3c36e792620e4bdf086467bd7745
Andrius Šilgalis <andrius.s@seo.lt>
2018-08-23 14:18:44 +0300

    Add test for AwsSsmSettingsProvider with single parameter

--- tests/src/Unit/Provider/AwsSsmSettingsProviderTest.php
+++ tests/src/Unit/Provider/AwsSsmSettingsProviderTest.php
@@ -8,6 +8,7 @@ use Aws\Result;
 use Aws\Ssm\SsmClient;
 use Helis\SettingsManagerBundle\Model\DomainModel;
 use Helis\SettingsManagerBundle\Model\SettingModel;
+use Helis\SettingsManagerBundle\Model\Type;
 use Helis\SettingsManagerBundle\Provider\AwsSsmSettingsProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -52,6 +53,39 @@ class AwsSsmSettingsProviderTest extends TestCase
         $settings = $settingsProvider->getSettings([DomainModel::DEFAULT_NAME]);
 
         $this->assertCount(0, $settings);
+    }
+
+    public function testGetSettingsWithSingleParameter(): void
+    {
+        $settingsProvider = $this->createSettingsProvider(['parameter_a']);
+
+        $awsResult = $this->createConfiguredMock(
+            Result::class,
+            [
+                'get' => [
+                    [
+                        'Name'  => 'Parameter A Name',
+                        'Value' => 'a_value',
+                    ],
+                ],
+            ]
+        );
+
+        $this->awsSsmClientMock
+            ->method('getParameters')
+            ->willReturnMap([
+                [['Names' => ['parameter_a']], $awsResult],
+            ]);
+
+        /** @var SettingModel[] $settings */
+        $settings = $settingsProvider->getSettings([DomainModel::DEFAULT_NAME]);
+
+        $this->assertCount(1, $settings);
+
+        $this->assertSame('Parameter A Name', $settings[0]->getName());
+        $this->assertSame(DomainModel::DEFAULT_NAME, $settings[0]->getDomain()->getName());
+        $this->assertSame(Type::STRING, $settings[0]->getType()->getValue());
+        $this->assertSame('a_value', $settings[0]->getData());
     }
 
     private function createSettingsProvider(array $parameterNames): AwsSsmSettingsProvider
