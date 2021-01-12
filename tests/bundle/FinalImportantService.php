496f23152b6b93afb1fe799709a6937d6ce805ce
Martynas Sudintas <martynas.s@helis.lt>
2021-01-12 12:29:13 +0200

    test setting aware tag with final class

---
+++ tests/bundle/FinalImportantService.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+use Helis\SettingsManagerBundle\Settings\Switchable\SwitchableTrait;
+
+final class FinalImportantService
+{
+    use SwitchableTrait;
+}
--- tests/src/Functional/DependencyInjection/Compiler/SettingsAwarePassTest.php
+++ tests/src/Functional/DependencyInjection/Compiler/SettingsAwarePassTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Helis\SettingsManagerBundle\Tests\Functional\DependencyInjection\Compiler;
 
+use App\FinalImportantService;
 use App\ImportantService;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
@@ -16,6 +17,15 @@ class SettingsAwarePassTest extends WebTestCase
         $this->loadFixtures([]);
 
         $service = $this->getContainer()->get(ImportantService::class);
+
+        $this->assertTrue($service->isEnabled());
+    }
+
+    public function testIsEnabledWithFinalClass()
+    {
+        $this->loadFixtures([]);
+
+        $service = $this->getContainer()->get(FinalImportantService::class);
 
         $this->assertTrue($service->isEnabled());
     }
