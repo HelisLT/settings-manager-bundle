243a9a798005953943acba2d503ae8c40097ce8b
Daumantas Batutis <daumantas.batutis@eneba.com>
2022-01-26 09:21:36 +0200

    Increase enqueue/enqueue to 0.10

--- src/Enqueue/Consumption/WarmupSettingsManagerExtension.php
+++ src/Enqueue/Consumption/WarmupSettingsManagerExtension.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Helis\SettingsManagerBundle\Enqueue\Consumption;
 
-use Enqueue\Consumption\Context;
-use Enqueue\Consumption\EmptyExtensionTrait;
-use Enqueue\Consumption\ExtensionInterface;
+use Enqueue\Consumption\Context\MessageReceived;
+use Enqueue\Consumption\MessageReceivedExtensionInterface;
 use Helis\SettingsManagerBundle\Settings\Traits\SettingsRouterAwareTrait;
 
-class WarmupSettingsManagerExtension implements ExtensionInterface
+class WarmupSettingsManagerExtension implements MessageReceivedExtensionInterface
 {
-    use EmptyExtensionTrait;
     use SettingsRouterAwareTrait;
 
     private $currentIteration;
@@ -28,7 +26,7 @@ class WarmupSettingsManagerExtension implements ExtensionInterface
         $this->divider = $divider;
     }
 
-    public function onPreReceived(Context $context)
+    public function onMessageReceived(MessageReceived $context): void
     {
         if (!$this->settingsRouter) {
             return;
