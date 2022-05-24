1ea6d9ebe96739faed124a1e914e7b2121819c08
Daumantas Batutis <daumantaz@gmail.com>
2022-05-24 14:05:16 +0300

    Fix settings administration (#97)
    * Fix domain template for twig v3
    
    * Remove no longer supported form option for Symfony 5
    
    Co-authored-by: Daumantas Batutis <daumantas.batutis@eneba.com>

---
+++ tests/src/Functional/Controller/DomainControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Helis\SettingsManagerBundle\Tests\Functional\Controller;
+
+use App\AbstractWebTestCase;
+
+class DomainControllerTest extends AbstractWebTestCase
+{
+    public function testIndexAction()
+    {
+        $client = $this->makeClient();
+        $this->loadFixtures([]);
+
+        $client->request('GET', '/domains');
+
+        $this->assertStatusCode(200, $client);
+    }
+}
