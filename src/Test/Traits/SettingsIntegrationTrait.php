4887759f25155c3f2becc2ed5fed8d947aa4d48b
Povilas Šivickis <povilas.sivickis@eneba.com>
2025-05-15 16:11:46 +0300

    make SettingsIntegrationTrait compatible with PHPUnit 12+

--- src/Test/Traits/SettingsIntegrationTrait.php
+++ src/Test/Traits/SettingsIntegrationTrait.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Helis\SettingsManagerBundle\Test\Traits;
 
 use Helis\SettingsManagerBundle\Test\Provider\SettingsProviderMock;
+use PHPUnit\Framework\Attributes\After;
 
 trait SettingsIntegrationTrait
 {
+    #[After]
     /**
      * @after
      */
