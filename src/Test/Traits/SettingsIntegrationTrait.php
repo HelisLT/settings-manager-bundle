ff5b4a00ad45c0e8b95d41ee4c8cd3fa1ffac373
pvsiv <povilas.sivickis@protonmail.com>
2025-05-16 11:45:17 +0300

    Make SettingsIntegrationTrait compatible with PHPUnit 12+ (#111)
    * make SettingsIntegrationTrait compatible with PHPUnit 12+
    
    * swap order of attribute and annotation
    
    ---------
    
    Co-authored-by: Povilas Šivickis <povilas.sivickis@eneba.com>

--- src/Test/Traits/SettingsIntegrationTrait.php
+++ src/Test/Traits/SettingsIntegrationTrait.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Helis\SettingsManagerBundle\Test\Traits;
 
 use Helis\SettingsManagerBundle\Test\Provider\SettingsProviderMock;
+use PHPUnit\Framework\Attributes\After;
 
 trait SettingsIntegrationTrait
 {
     /**
      * @after
      */
+    #[After]
     public function tearDownSettings(): void
     {
         SettingsProviderMock::clear();
